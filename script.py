bd3e05f0c2e8a7751b730610cfa55c30d013ae04
jeremy_zeidner <jeremy_zeidner@yahoo.com>
2011-10-08 21:06:31 -0400

    fsdfsd

--- script.py
+++ script.py
@@ -3,11 +3,11 @@ import pymongo
 import datetime
 import time
 import datetime
+import math
 from bson.code import Code
 
-es = { "symbol" : "ES" , "sessionTime" : {
-        "sessionStartTime": time.strptime("9:30:00","%H:%M:%S"),
-        "sessionEndTime" : time.strptime("16:15:00","%H:%M:%S")}} 
+es = {"symbol" : "ES" ,  "sessionStartTime" : datetime.time(hour=9,minute=30,second=00) ,
+                          "sessionEndTime" : datetime.time(hour=16,minute=15,second=00) }
 
 def GetMap():
     map = Code("function () {"\
@@ -30,14 +30,14 @@ def GetQuery(startTime,endTime):
      return query
     
     
-def ProcessFile(fileName,date):
+def ProcessFile(fileName,date,symbol):
     """Takes a file name and inserts the tick data into the database"""
     from time import mktime
     from datetime import datetime
     
     Myfile = open("./" + fileName)
     print "first line %s" % (Myfile.readline())
-    coll = GetConnection()["es_ticks_" + date]
+    coll = GetConnection()[symbol + "_ticks_" + date]
     index = 0
     for line in Myfile:
         if index == 10000:
@@ -57,45 +57,66 @@ def GetConnection():
     db = connection['ES_DATA']
     #collection = db.es_ticks
     return db
-def GetRange(symbol):
-    return range(1,15)
+def GetRange(symbol,minutes):
+    #get total length of session and divide by minutes
+    #need a fake date to subtract
+    start = datetime.datetime(1900,1,1,symbol["sessionStartTime"].hour,symbol["sessionStartTime"].minute,symbol["sessionStartTime"].second)
+    end = datetime.datetime(1900,1,1,symbol["sessionEndTime"].hour,symbol["sessionEndTime"].minute,symbol["sessionEndTime"].second)
+ 
+    diff =  end - start
+    numOfUnitsOfMinutes =  float((diff.seconds /60 )) / float(minutes)
+    x = math.ceil(numOfUnitsOfMinutes)
+    return range(1,x + 1)
 
-def CreateRangeForMinutes(symbol,minutesPerBar,date):
+def CreateStatsForRangeOfMinutes(symbol,minutesPerBar,date):
     from datetime import timedelta
     
     conn = GetConnection()
     coll = conn.es_ticks
     rollup = conn.rollup
-    startTime = datetime.datetime(2011,4,4,9,30,00)
-    endTime = datetime.datetime(2011,4,4,4,15,00)
+   
 
     tempDate = datetime.datetime(2011,4,4,9,30,00)
 
-    for i in GetRange(symbol): # 15 half hour sessions and 1 15 minute
-        startTimeDelta  = timedelta(minutes=minutesPerBar * i)
-        endTimeDelta = timedelta(minutes=minutesPerBar * (i - 1))
-        if i == 14: # last one is only 15 minutes
-            temp = minutesPerBar * i
-            startTimeDelta  = datetime.timedelta(minutes= temp - 15)
-        else :
-            startTimeDelta  = datetime.timedelta(minutes=minutesPerBar * i)
-        startTime = tempDate + endTimeDelta
-        endTime = tempDate + startTimeDelta
-        print "start: %s end: %s" % (startTime,endTime)
-        result = coll.map_reduce(GetMap(),GetReduce(),"myresult",GetQuery(startTime,endTime))
-        print result
-        for m in result.find():
-            print m
-            #insert the volume at price for that given period
-            doc = {'start_time': startTime,
-                   'end_time': endTime,
-                   'v' : m['value'],
-                   'price': m['_id']}
-            rollup.insert(doc)
-        conn.drop_collection("myresult")
+    startTime = date + timedelta(hours=symbol["sessionStartTime"].hour,minutes=symbol["sessionStartTime"].minute,seconds=symbol["sessionStartTime"].second )
+
+    for i in GetRange(symbol,30): # 15 half hour sessions and 1 15 minute
+##        startTimeDelta  = timedelta(minutes=minutesPerBar * i)
+##        endTimeDelta = timedelta(minutes=minutesPerBar * (i - 1))
+##        if i == 14: # last one is only 15 minutes
+##            temp = minutesPerBar * i
+##            startTimeDelta  = timedelta(minutes= temp - 15)
+##        else :
+##            startTimeDelta  = timedelta(minutes=minutesPerBar * i)
+##        startTime = tempDate + endTimeDelta
+##        endTime = tempDate + startTimeDelta
+       
+        rangeStartTime  = startTime + timedelta(minutes=minutesPerBar* (i-1))
+        rangeEndTime = rangeStartTime + timedelta(minutes=minutesPerBar,seconds=-1)
+
+        
+        if rangeEndTime.time() > symbol["sessionEndTime"]:
+            rangeEndTime = symbol["sessionEndTime"]
+
+            
+        print "start: %s end: %s" % (rangeStartTime,rangeEndTime)
+
+       
+##        result = coll.map_reduce(GetMap(),GetReduce(),"myresult",GetQuery(startTime,endTime))
+##        print result
+##        for m in result.find():
+##            print m
+##            #insert the volume at price for that given period
+##            doc = {'start_time': startTime,
+##                   'end_time': endTime,
+##                   'v' : m['value'],
+##                   'price': m['_id']}
+##            rollup.insert(doc)
+##        conn.drop_collection("myresult")
 
 
     # get all records in the range and get the volume at each price
 if __name__ == '__main__' :
-    CreateRangeForMinutes(es,30,datetime.datetime(2011,4,4))
-    #ProcessFile("data.txt","2011-4-4") 
+    CreateStatsForRangeOfMinutes(es,30,datetime.datetime(2011,4,4))
+    #ProcessFile("data.txt","2011-4-4","es") 
+    #GetRange(es,30)
