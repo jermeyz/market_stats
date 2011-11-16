67a59ad4be21dcb97670b38f648015e2bcd3ea6d
jeremy_zeidner <jeremy_zeidner@yahoo.com>
2011-11-16 14:34:25 -0500

    adding rails app

---
+++ web/market_stats/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MarketStats::Application.config.secret_token = '2b43a8be80129c4f97de8e75588825ff98d87f8cbff4b81a71ad1bbe2cceaaf4b804373c7cd5b812891d6c443f04dfa938affa3d8f123704d4021c5c207a5512'
