a1bfb2bc431801a9a96ea9bff9025f5f0b868283
jeremy_zeidner <jeremy_zeidner@yahoo.com>
2012-01-02 01:03:53 -0500

    test

--- market_stats/app/controllers/stats_controller.rb
+++ market_stats/app/controllers/stats_controller.rb
@@ -5,10 +5,24 @@ class StatsController < ApplicationController
     
     	@collection = db["ES-30-minute-bars"]
 
-	@doc = @collection.find_one()
+	#@doc = @collection.find_one()
+
+
 
       respond_to do |format|
       	format.html # index.html.erb
+      end
+  end
+  def daily
+
+       db = Mongo::Connection.new(APP_CONFIG["host"],APP_CONFIG["port"]).db(APP_CONFIG["database"])
+    	auth = db.authenticate(APP_CONFIG["username"],APP_CONFIG["password"])
+    
+    	@collection = db["ES-30-minute-bars"]
+
+	@doc = @collection.find_one()
+      respond_to do |format|
+      	format.json { render :json => @doc.to_json() }
       end
   end
 end
